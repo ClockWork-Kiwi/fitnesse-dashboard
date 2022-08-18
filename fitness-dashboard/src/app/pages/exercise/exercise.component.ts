e6b3b93e5bcdddb721e97b42c1627d0a6eff056e
Jesse <jessewilshier98@gmail.com>
2022-08-19 10:42:57 +1200

    Basic mockup of exercise screen

--- fitness-dashboard/src/app/pages/exercise/exercise.component.ts
+++ fitness-dashboard/src/app/pages/exercise/exercise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faMinusCircle, faPlusCircle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-exercise',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise.component.scss']
 })
 export class ExerciseComponent implements OnInit {
+
+  public addIcon = faPlusCircle;
+  public removeIcon = faMinusCircle;
 
   constructor() { }
 
