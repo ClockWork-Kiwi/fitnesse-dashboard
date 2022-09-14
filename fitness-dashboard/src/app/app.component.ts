08a52a4ce1ce56443db21b11f6d47ebbc6140dd9
Jesse <jessewilshier98@gmail.com>
2022-09-14 13:12:03 +1200

    Navigation buttons highlight when their respective screen is showing

--- fitness-dashboard/src/app/app.component.ts
+++ fitness-dashboard/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subject} from "rxjs";
+import {filter, takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fitness-dashboard';
+export class AppComponent implements OnInit, OnDestroy {
+  public title = 'fitness-dashboard';
+  private componentDestruction$ = new Subject();
+  public activeRoute = '';
 
   public routes = [
     { label: 'Dashboard', route: 'dashboard' },
@@ -22,5 +26,18 @@ export class AppComponent {
 
   public navigateToRoute(route) {
     this.router.navigateByUrl(route);
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      takeUntil(this.componentDestruction$),
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe((e: NavigationEnd) => {
+      this.activeRoute = e.url.slice(1, e.url.length);
+    });
+  }
+
+  ngOnDestroy() {
+    this.componentDestruction$.next(true);
   }
 }
