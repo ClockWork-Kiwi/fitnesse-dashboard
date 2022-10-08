f7256db62e0f40d520c0246bd235d03fc1bbffa6
Jesse <jessewilshier98@gmail.com>
2022-10-08 19:29:48 +1300

    Imported chartjs into project. Created dummy chart to be sure it works

--- fitness-dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ fitness-dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart, registerables} from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,21 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const thisWeekContext = document.getElementById('thisWeekChart') as any;
+    const thisWeekData = [
+      {
+        data: [500, 500],
+      }
+    ];
+    const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const thisWeekChart = new Chart(thisWeekContext, {
+      type: 'line',
+      data: {
+        labels: weekdays,
+        datasets: thisWeekData
+      }
+    });
+
   }
 
 }
