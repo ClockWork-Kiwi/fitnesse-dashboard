8904c868784e97c174df33d5ce8302d33b87f3bb
Jesse <jessewilshier98@gmail.com>
2022-10-10 12:27:13 +1300

    Adjusted layout of dashboard component. Created count-up directive

---
+++ fitness-dashboard/src/app/directives/count-up.directive.ts
@@ -0,0 +1,80 @@
+import {Directive, ElementRef, Input, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {animationFrameScheduler, BehaviorSubject, combineLatest, interval, Subject} from 'rxjs';
+import {distinctUntilChanged, endWith, map, switchMap, takeUntil, takeWhile} from 'rxjs/operators';
+
+/**
+ * Quadratic Ease-Out Function: f(x) = x * (2 - x)
+ */
+const easeOutQuad = (x: number): number => x * (2 - x);
+
+@Directive({
+  selector: '[countUp]',
+})
+export class CountUpDirective implements OnInit, OnDestroy {
+  private destroy$ = new Subject();
+
+  private readonly count$ = new BehaviorSubject(0);
+  private readonly duration$ = new BehaviorSubject(2000);
+
+  private readonly currentCount$ = combineLatest([
+    this.count$,
+    this.duration$,
+  ]).pipe(
+    switchMap(([count, duration]) => {
+      // get the time when animation is triggered
+      const startTime = animationFrameScheduler.now();
+
+      return interval(0, animationFrameScheduler).pipe(
+        // calculate elapsed time
+        map(() => animationFrameScheduler.now() - startTime),
+        // calculate progress
+        map((elapsedTime) => elapsedTime / duration),
+        // complete when progress is greater than 1
+        takeWhile((progress) => progress <= 1),
+        // apply quadratic ease-out function
+        // for faster start and slower end of counting
+        map(easeOutQuad),
+        // calculate current count
+        map((progress) => Math.round(progress * count)),
+        // make sure that last emitted value is count
+        endWith(count),
+        distinctUntilChanged()
+      );
+    }),
+  );
+
+  @Input('countUp')
+  set count(count: number) {
+    this.count$.next(count);
+  }
+
+  @Input()
+  set duration(duration: number) {
+    this.duration$.next(duration);
+  }
+
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2,
+  ) {}
+
+  ngOnInit(): void {
+    this.displayCurrentCount();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
+
+  private displayCurrentCount(): void {
+    this.currentCount$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((currentCount) => {
+        this.renderer.setProperty(
+          this.elementRef.nativeElement,
+          'innerHTML',
+          currentCount
+        );
+      });
+  }
+}
